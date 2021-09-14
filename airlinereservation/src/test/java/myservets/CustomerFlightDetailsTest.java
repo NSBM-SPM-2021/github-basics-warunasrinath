5798af3422aac27acfa3b5cca7e64b6a71662799
waruna srinath <56961480+warunasrinath@users.noreply.github.com>
2021-09-15 01:11:39 +0530

    JUnit testing
    The first commitment after JUnit testing

---
+++ airlinereservation/src/test/java/myservets/CustomerFlightDetailsTest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myservets;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Asus
+ */
+public class CustomerFlightDetailsTest {
+    
+    public CustomerFlightDetailsTest() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+    
+}
